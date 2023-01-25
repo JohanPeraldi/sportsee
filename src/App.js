051f2169695ce8dc5ac2730c9432c17dc452956c
Johan Peraldi <johan.peraldi@gmail.com>
2023-01-25 14:22:42 +0100

    Add Axios and use it in App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import AverageSessionLength from './components/AverageSessionLength/AverageSessionLength';
 import DailyActivity from './components/DailyActivity/DailyActivity';
 import Header from './components/Header/Header';
@@ -7,6 +10,27 @@ import Score from './components/Score/Score';
 import styles from './App.module.css';
 
 function App() {
+  const [data, setData] = useState();
+  const baseUrl = 'http://localhost:3000/user/';
+  const { id } = useParams();
+  const url = baseUrl + id;
+
+  const fetchData = useCallback(async () => {
+    const response = await axios(url);
+    setData(response.data);
+  }, [url]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (data) {
+    console.group('*** DATA ***');
+    console.log('User id: ', data.data.id);
+    console.log('User first name: ', data.data.userInfos.firstName);
+    console.groupEnd();
+  }
+
   return (
     <div className={styles.app}>
       <Header />
