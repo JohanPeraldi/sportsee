14763f7aebb65125b11384e266eae5fc9faaf693
Johan Peraldi <johan.peraldi@gmail.com>
2023-01-25 15:54:59 +0100

    Export axios to api file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+//import axios from 'axios';
+import { getUserInfo } from './api/api';
 import AverageSessionLength from './components/AverageSessionLength/AverageSessionLength';
 import DailyActivity from './components/DailyActivity/DailyActivity';
 import Header from './components/Header/Header';
@@ -11,14 +12,15 @@ import styles from './App.module.css';
 
 function App() {
   const [data, setData] = useState();
-  const baseUrl = 'http://localhost:3000/user/';
+  //const baseUrl = 'http://localhost:3000/user/';
   const { id } = useParams();
-  const url = baseUrl + id;
+  //const url = baseUrl + id;
 
   const fetchData = useCallback(async () => {
-    const response = await axios(url);
-    setData(response.data);
-  }, [url]);
+    //const response = await axios(url);
+    const res = await getUserInfo(id);
+    setData(res);
+  }, [id]);
 
   useEffect(() => {
     fetchData();
---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3000/user/';
+/* Endpoints */
+//const userInfoUrl = baseUrl + id;
+//const activityUrl = userInfoUrl + '/activity';
+//const averageSessionsUrl = userInfoUrl + '/average-sessions';
+//const performanceUrl = userInfoUrl + '/performance';
+
+export async function getUserInfo(id) {
+  const response = await axios(baseUrl + id);
+
+  return response.data;
+}
